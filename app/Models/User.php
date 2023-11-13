e0dc2ffd7126c2f11b0fba08368d945e5fb24959
Diamond <fortunes.teller@outlook.com>
2023-11-13 16:41:57 +0900

    Add User model

---
+++ app/Models/User.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class User extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'firstname',
+        'lastname',
+        'hobby'
+    ];
+
+    protected $dates = ['deleted_at']; 
+}
